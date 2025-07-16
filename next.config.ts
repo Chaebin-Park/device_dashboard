f7fa75875db93046209fe19871f6d430f6d0393c
chaebin <chaebin@chaebinui-MacBookPro.local>
2025-07-16 11:46:42 +0900

    ci

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      }
+    }
+    return config
+  },
+
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  }
 };
 
 export default nextConfig;
